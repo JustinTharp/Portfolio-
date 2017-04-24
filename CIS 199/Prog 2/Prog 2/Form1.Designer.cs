11220db3a863001be2f7064f6a061653b5fbc106
JustinTharp <justintharp94@gmail.com>
2017-04-24 00:40:30 -0400

    Add files via upload

---
+++ CIS 199/Prog 2/Prog 2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Prog_2
+{
+    partial class Prog2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastnametextBox = new System.Windows.Forms.TextBox();
+            this.lastnamelabel = new System.Windows.Forms.Label();
+            this.seniorradioButton = new System.Windows.Forms.RadioButton();
+            this.juniorradioButton = new System.Windows.Forms.RadioButton();
+            this.sophomoreradioButton = new System.Windows.Forms.RadioButton();
+            this.freshmanradioButton = new System.Windows.Forms.RadioButton();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lastnametextBox
+            // 
+            this.lastnametextBox.Location = new System.Drawing.Point(67, 63);
+            this.lastnametextBox.Name = "lastnametextBox";
+            this.lastnametextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastnametextBox.TabIndex = 0;
+            // 
+            // lastnamelabel
+            // 
+            this.lastnamelabel.AutoSize = true;
+            this.lastnamelabel.Location = new System.Drawing.Point(2, 66);
+            this.lastnamelabel.Name = "lastnamelabel";
+            this.lastnamelabel.Size = new System.Drawing.Size(59, 13);
+            this.lastnamelabel.TabIndex = 1;
+            this.lastnamelabel.Text = "Last name:";
+            // 
+            // seniorradioButton
+            // 
+            this.seniorradioButton.AutoSize = true;
+            this.seniorradioButton.Location = new System.Drawing.Point(220, 89);
+            this.seniorradioButton.Name = "seniorradioButton";
+            this.seniorradioButton.Size = new System.Drawing.Size(55, 17);
+            this.seniorradioButton.TabIndex = 2;
+            this.seniorradioButton.Text = "Senior";
+            this.seniorradioButton.UseVisualStyleBackColor = true;
+            // 
+            // juniorradioButton
+            // 
+            this.juniorradioButton.AutoSize = true;
+            this.juniorradioButton.Location = new System.Drawing.Point(220, 66);
+            this.juniorradioButton.Name = "juniorradioButton";
+            this.juniorradioButton.Size = new System.Drawing.Size(53, 17);
+            this.juniorradioButton.TabIndex = 3;
+            this.juniorradioButton.TabStop = true;
+            this.juniorradioButton.Text = "Junior";
+            this.juniorradioButton.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreradioButton
+            // 
+            this.sophomoreradioButton.AutoSize = true;
+            this.sophomoreradioButton.Location = new System.Drawing.Point(220, 43);
+            this.sophomoreradioButton.Name = "sophomoreradioButton";
+            this.sophomoreradioButton.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreradioButton.TabIndex = 4;
+            this.sophomoreradioButton.TabStop = true;
+            this.sophomoreradioButton.Text = "Sophomore";
+            this.sophomoreradioButton.UseVisualStyleBackColor = true;
+            // 
+            // freshmanradioButton
+            // 
+            this.freshmanradioButton.AutoSize = true;
+            this.freshmanradioButton.Checked = true;
+            this.freshmanradioButton.Location = new System.Drawing.Point(220, 20);
+            this.freshmanradioButton.Name = "freshmanradioButton";
+            this.freshmanradioButton.Size = new System.Drawing.Size(71, 17);
+            this.freshmanradioButton.TabIndex = 5;
+            this.freshmanradioButton.TabStop = true;
+            this.freshmanradioButton.Text = "Freshman";
+            this.freshmanradioButton.UseVisualStyleBackColor = true;
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.Location = new System.Drawing.Point(142, 136);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(75, 23);
+            this.submitbutton.TabIndex = 6;
+            this.submitbutton.Text = "Submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            this.submitbutton.Click += new System.EventHandler(this.submitbutton_Click);
+            // 
+            // Prog2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 192);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.freshmanradioButton);
+            this.Controls.Add(this.sophomoreradioButton);
+            this.Controls.Add(this.juniorradioButton);
+            this.Controls.Add(this.seniorradioButton);
+            this.Controls.Add(this.lastnamelabel);
+            this.Controls.Add(this.lastnametextBox);
+            this.Name = "Prog2";
+            this.Text = "Prog 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lastnametextBox;
+        private System.Windows.Forms.Label lastnamelabel;
+        private System.Windows.Forms.RadioButton seniorradioButton;
+        private System.Windows.Forms.RadioButton juniorradioButton;
+        private System.Windows.Forms.RadioButton sophomoreradioButton;
+        private System.Windows.Forms.RadioButton freshmanradioButton;
+        private System.Windows.Forms.Button submitbutton;
+    }
+}
+
---
+++ CIS 199/Prog 2/Prog 2/Form1.cs
@@ -0,0 +1,250 @@
+﻿// Justin Tharp
+// Program 2
+// Due Date: March 9, 2015
+// CIS 199-01
+// This program allows the user to select their year and enter their last name to determine what day and time they will be eligible
+// to register for the next semester's classes. It works by sorting each student first by which year they are in (which radio button
+// they select) and then uses character variables to determine when they can register based on the name they enter in the textbox.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_2
+{
+    public partial class Prog2 : Form
+    {
+        public Prog2()
+        {
+            InitializeComponent();
+        }
+        // Program runs when the submit button is clicked.
+        private void submitbutton_Click(object sender, EventArgs e)
+        {
+            // If statement to determine if the textbox is empty.
+            if (string.IsNullOrEmpty(lastnametextBox.Text))
+            {
+                MessageBox.Show("Please enter your last name."); // If textbox is empty, show messagebox.
+            }
+            else // Otherwise, run program.
+            {
+                char letter; // Define standard variable for letter. 
+                letter = 'y';
+
+                // String text from textbox as name.
+                string name = lastnametextBox.Text;
+
+                // Define variable letter equal to the first character extracted from text.
+                letter = name[0];
+
+                // Convert any text entered to uppercase so it is compatible with the program.
+                letter = char.ToUpper(letter);
+
+                // Define each character variable.
+                // Make each character variable equal to the letter it defines. 
+                char letterA;
+                letterA = 'A';
+
+                char letterB;
+                letterB = 'B';
+
+                char letterC;
+                letterC = 'C';
+
+                char letterD;
+                letterD = 'D';
+
+                char letterE;
+                letterE = 'E';
+
+                char letterF;
+                letterF = 'F';
+
+                char letterG;
+                letterG = 'G';
+
+                char letterI;
+                letterI = 'I';
+
+                char letterJ;
+                letterJ = 'J';
+
+                char letterL;
+                letterL = 'L';
+
+                char letterM;
+                letterM = 'M';
+
+                char letterO;
+                letterO = 'O';
+
+                char letterP;
+                letterP = 'P';
+
+                char letterQ;
+                letterQ = 'Q';
+
+                char letterR;
+                letterR = 'R';
+
+                char letterS;
+                letterS = 'S';
+
+                char letterT;
+                letterT = 'T';
+
+                char letterV;
+                letterV = 'V';
+
+                char letterW;
+                letterW = 'W';
+
+                char letterZ;
+                letterZ = 'Z';
+
+               
+                // If senior button is checked...
+                if (seniorradioButton.Checked)
+                {
+                    if (letter >= letterT && letter <= letterZ) // If first letter is T, Z or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 1  8:30am"); // Display messagebox. 
+                    }
+                    else if (letter >= letterA && letter <= letterD) // if not, If first letter is A, D or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 1  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterE && letter <= letterI) // if not, If first letter is E, I or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 1  11:30am"); // Display messagebox. 
+                    }
+                    else if (letter >= letterJ && letter <= letterO) // if not, If first letter is J, O or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 1  2:00pm"); // Display messagebox. 
+                    }
+                    else // Else (P, S or inbetween.)
+                    {
+                        MessageBox.Show("Wednesday, April 1  4:00pm"); // Display messagebox. 
+                    }
+                }
+                // If junior button is checked...
+                else if (juniorradioButton.Checked)
+                {
+                    if (letter >= letterT && letter <= letterZ) // If first letter is T, Z or inbetween...
+                    {
+                        MessageBox.Show("Thursday, April 2  8:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterA && letter <= letterD) // if not, If first letter is A, D or inbetween...
+                    {
+                        MessageBox.Show("Thursday, April 2  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterE && letter <= letterI) // if not, If first letter is E, I or inbetween...
+                    {
+                        MessageBox.Show("Thursday, April 2  11:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterJ && letter <= letterO) // if not, If first letter is J, O or inbetween...
+                    {
+                        MessageBox.Show("Thursday, April 2  2:00pm"); // Display messagebox.
+                    }
+                    else // Else (P, S or inbetween)
+                    {
+                        MessageBox.Show("Thursday, April 2  4:00pm"); // Display messagebox.
+                    }
+                }
+                // If sophomore button is checked...
+                else if (sophomoreradioButton.Checked)
+                {
+                    if (letter >= letterT && letter <= letterV) // If first letter is T, V or inbetween...
+                    {
+                        MessageBox.Show("Friday, April 3  8:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterW && letter <= letterZ) // if not, If first letter is W, Z or inbetween...
+                    {
+                        MessageBox.Show("Friday, April 3  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterA && letter <= letterB) // if not, If first letter is A or B...
+                    {
+                        MessageBox.Show("Friday, April 3  11:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterC && letter <= letterD) // if not, If first letter is C or D...
+                    {
+                        MessageBox.Show("Friday, April 3  2:00pm"); // Display messagebox.
+                    }
+                    else if (letter >= letterE && letter <= letterF) // if not, If first letter is E or F...
+                    {
+                        MessageBox.Show("Friday, April 3  4:00pm"); // Display messagebox.
+                    }
+                    else if (letter >= letterG && letter <= letterI) // if not, If first letter is G, I or inbetween...
+                    {
+                        MessageBox.Show("Monday, April 6  8:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterJ && letter <= letterL) // if not, If first letter is J, L or inbetween...
+                    {
+                        MessageBox.Show("Monday, April 6  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterM && letter <= letterO) // if not, If first letter is M, O or inbetween...
+                    {
+                        MessageBox.Show("Monday, April 6  11:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterP && letter <= letterQ) // if not, If first letter is P or Q...
+                    {
+                        MessageBox.Show("Monday, April 6  2:00pm"); // Display messagebox.
+                    }
+                    else // Else (R or S)
+                    {
+                        MessageBox.Show("Monday, April 6  4:00pm"); // Display messagebox.
+                    }
+                }
+                // If none of above, Freshman button checked.
+                else
+                {
+                    if (letter >= letterT && letter <= letterV) // If first letter is T, V or inbetween...
+                    {
+                        MessageBox.Show("Tuesday, April 7  8:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterW && letter <= letterZ) // if not, If first letter is W, Z or inbetween...
+                    {
+                        MessageBox.Show("Tuesday, April 7  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterA && letter <= letterB) // if not, If first letter is A or B...
+                    {
+                        MessageBox.Show("Tuesday, April 7  11:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterC && letter <= letterD) // if not, If first letter is C, or D...
+                    {
+                        MessageBox.Show("Tuesday, April 7  2:00pm"); // Display messagebox.
+                    }
+                    else if (letter >= letterE && letter <= letterF) // if not, If first letter is E or F...
+                    {
+                        MessageBox.Show("Tuesday, April 7  4:00pm"); // Display messagebox.
+                    }
+                    else if (letter >= letterG && letter <= letterI) // if not, If first letter is G, I or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 8  8:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterJ && letter <= letterL) // if not,If first letter is J, L or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 8  10:00am"); // Display messagebox.
+                    }
+                    else if (letter >= letterM && letter <= letterO) // if not, If first letter is M, O or inbetween...
+                    {
+                        MessageBox.Show("Wednesday, April 8  11:30am"); // Display messagebox.
+                    }
+                    else if (letter >= letterP && letter <= letterQ) // if not, If first letter is P, or Q...
+                    {
+                        MessageBox.Show("Wednesday, April 8  2:00pm"); // Display messagebox.
+                    }
+                    else // Else (R or S)
+                    {
+                        MessageBox.Show("Wednesday, April 8  4:00pm"); // Display messagebox.
+                    }
+                }
+            }
+        }
+    }
+}
